09c3e7ea5111c2506b1b16685df8aded27913a28
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-11 16:13:40 +0400

    added AuthForm component to index.vue page

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import AuthForm from "@/components/AuthForm.vue";
+
+definePageMeta({ middleware: "public" });
+
+const { status } = useAuth();
+</script>
+
+<template>
+  <div
+    class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100"
+  >
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+      <NuxtImg
+        height="48"
+        width="48"
+        class="mx-auto w-auto"
+        src="/images/logo.png"
+        alt="Logo"
+      />
+      <h2
+        class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900"
+      >
+        Sign in to your account
+      </h2>
+    </div>
+    <AuthForm v-if="status !== 'authenticated'" />
+  </div>
+</template>
