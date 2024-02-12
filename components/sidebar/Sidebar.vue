9c174161e00440919282d6f7184f6a697e92bb38
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-12 16:01:31 +0400

    created sidebar component with mobile footer for smaller viewports

---
+++ components/sidebar/Sidebar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import DesktopSidebar from "./DesktopSidebar.vue";
+import MobileFooter from "./MobileFooter.vue";
+</script>
+
+<template>
+  <div class="h-full">
+    <DesktopSidebar />
+    <MobileFooter />
+    <main class="lg:pl-20 h-full">
+      <slot />
+    </main>
+  </div>
+</template>
