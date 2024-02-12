0407d9ceb2957b5418e9b62ee3d4a5faf8112e7d
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-12 16:06:45 +0400

    added pusher and headless ui library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@sidebase/nuxt-auth', 'nuxt3-notifications', '@pinia/nuxt', '@nuxtjs/cloudinary', '@formkit/nuxt', '@nuxt/image'],
-
+  modules: ['@nuxtjs/tailwindcss', '@sidebase/nuxt-auth', 'nuxt-headlessui', 'nuxt3-notifications', '@pinia/nuxt', '@nuxtjs/cloudinary', '@formkit/nuxt', '@nuxt/image'],
+  runtimeConfig: {
+    public: {},
+    githubClientId: process.env.GITHUB_CLIENT_ID,
+    githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+    nuxtSecret: process.env.NUXTAUTH_SECRET
+  },
   auth: {
     provider: {
       type: 'authjs'
