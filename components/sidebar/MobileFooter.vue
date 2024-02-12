60d237614cc3278f91802c118ebb72ba05d26dbd
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-12 16:03:46 +0400

    created MobileFooter and MobileItem components

---
+++ components/sidebar/MobileFooter.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import MobileItem from "./MobileItem.vue";
+import { useConversation } from "@/composables/useConversation";
+const { signOut } = useAuth();
+const { isOpen } = useConversation();
+
+const routes = useRoutes();
+
+const onClick = (href: string) => {
+  if (href === "#") {
+    signOut();
+  }
+};
+</script>
+
+<template>
+  <div
+    v-if="!isOpen"
+    class="fixed justify-between w-full bottom-0 z-40 flex items-center bg-white border-t-[1px] lg:hidden"
+  >
+    <MobileItem
+      v-for="route in routes"
+      :key="route.href"
+      :href="route.href"
+      :active="route.active"
+      :icon="route.icon"
+      @click="onClick"
+    />
+  </div>
+</template>
---
+++ components/sidebar/MobileItem.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import clsx from "clsx";
+const emit = defineEmits(["click"]);
+
+interface MobileItemProps {
+  href: string;
+  icon: any;
+  active?: boolean;
+}
+const { href, icon, active } = defineProps<MobileItemProps>();
+</script>
+
+<template>
+  <NuxtLink
+    @click="emit('click')"
+    :href="href"
+    :class="
+      clsx(
+        `
+        group 
+        flex 
+        gap-x-3 
+        text-sm 
+        leading-6 
+        font-semibold 
+        w-full 
+        justify-center 
+        p-4 
+        text-gray-500 
+        hover:text-black 
+        hover:bg-gray-100
+      `,
+        active && 'bg-gray-100 text-black'
+      )
+    "
+  >
+    <component :is="icon" class="h-6 w-6" />
+  </NuxtLink>
+</template>
