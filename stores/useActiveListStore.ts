f9195e1cce6b47d9b4e91b38882bd232979535ab
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-13 10:40:50 +0400

    created active list store

---
+++ stores/useActiveListStore.ts
@@ -0,0 +1,24 @@
+import { defineStore } from "pinia";
+
+export const useActiveListStore = defineStore("main", () => {
+  const members = ref<string[]>([]);
+
+  function add(id: string) {
+    const newMembers = [...members.value, id];
+    set(newMembers)
+  }
+
+  function remove(id: string) {
+    const filteredMembers = members.value.filter((memberId) => memberId !== id);
+    set(filteredMembers)
+  }
+
+  function set(ids: string[]) {
+    members.value = ids;
+  }
+
+
+  return {
+    members
+  };
+});
