64ed27a5086820ee3feb597f6ae0ce8ffc958cff
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-13 10:37:35 +0400

    created Sidebar container component

--- components/sidebar/Sidebar.vue
+++ components/sidebar/Sidebar.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 import DesktopSidebar from "./DesktopSidebar.vue";
 import MobileFooter from "./MobileFooter.vue";
+
+const currentUser = ref();
+
+const res = await $fetch("/api/user", {
+  method: "GET",
+});
+if (res) {
+  currentUser.value = res;
+}
 </script>
 
 <template>
   <div class="h-full">
-    <DesktopSidebar />
+    <DesktopSidebar :currentUser="currentUser" />
     <MobileFooter />
     <main class="lg:pl-20 h-full">
       <slot />
