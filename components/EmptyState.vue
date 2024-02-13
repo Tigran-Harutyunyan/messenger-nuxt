78c869ce66b434738c3e45100c5afa5dfd91a36c
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-13 10:41:42 +0400

    created EmptyState component

---
+++ components/EmptyState.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="px-4 py-10 sm:px-6 lg:px-8 lg:py-6 h-full flex justify-center items-center bg-gray-100"
+  >
+    <div class="text-center items-center flex flex-col">
+      <h3 class="mt-2 text-2xl font-semibold text-gray-900">
+        Select a chat or start a new conversation
+      </h3>
+    </div>
+  </div>
+</template>
