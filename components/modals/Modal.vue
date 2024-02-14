41ea9b525c5f3622298107e18af4f1f68e35cf03
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-14 15:04:56 +0400

    created Modal component out of HeadlessUl dialog component

---
+++ components/modals/Modal.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+} from "@headlessui/vue";
+import ModalCloseIcon from "~/components/ui/icons/ModalCloseIcon.vue";
+
+const emit = defineEmits(["close"]);
+
+interface ModalProps {
+  isOpen?: boolean;
+}
+
+const { isOpen } = defineProps<ModalProps>();
+</script>
+
+<template>
+  <TransitionRoot appear :show="isOpen" as="template">
+    <Dialog as="div" class="relative z-50" @close="emit('close')">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div
+          class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+        />
+      </TransitionChild>
+
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center text-center sm:p-0"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all w-full sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+              ><span
+                class="p-1 border-0 hover:opacity-70 transition absolute right-3 top-5"
+                @click="emit('close')"
+              >
+                <ModalCloseIcon
+                  class="w-[18px] h-[18px] hover:opacity-80 cursor-pointer"
+                />
+              </span>
+              <slot />
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
