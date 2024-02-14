3e4b2c645cc7748740054e10ec96ae5b77f9b1e5
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-14 15:06:39 +0400

    added conversations and users container components

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import EmptyState from "@/components/EmptyState.vue";
 
-definePageMeta({ middleware: "auth", layout: "conversations" });
+definePageMeta({ middleware: "auth", layout: "users" });
 </script>
 
 <template>
