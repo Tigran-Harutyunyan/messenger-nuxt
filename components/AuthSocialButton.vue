d12d4245e014a2ef75207b76138bf151f43cd1a5
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-15 18:29:57 +0400

    created social button

---
+++ components/AuthSocialButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface AuthSocialButtonProps {
+  icon: any;
+}
+const { icon } = defineProps<AuthSocialButtonProps>();
+</script>
+
+<template>
+  <button
+    type="button"
+    class="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
+  >
+    <component :is="icon" />
+  </button>
+</template>
