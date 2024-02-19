8a35cf6c8c2b44a190a8aa247011f6a07088749d
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-19 14:15:50 +0400

    don`t show empy component on small screen

--- components/EmptyState.vue
+++ components/EmptyState.vue
@@ -3,7 +3,7 @@
     class="px-4 py-10 sm:px-6 lg:px-8 lg:py-6 h-screen flex justify-center items-center bg-gray-100"
   >
     <div class="text-center items-center flex flex-col">
-      <h3 class="mt-2 text-2xl font-semibold text-gray-900">
+      <h3 class="mt-2 text-2xl font-semibold text-gray-900 hidden lg:block">
         Select a chat or start a new conversation
       </h3>
     </div>
