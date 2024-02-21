7469a1713ed8840fa0b5a58740a6f881c9f0f3e0
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-21 14:56:52 +0400

    removed github provider

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,5 +1,4 @@
 import CredentialsProvider from 'next-auth/providers/credentials';
-import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 import { NuxtAuthHandler } from '#auth';
 import { compare } from "bcrypt-ts";
@@ -10,16 +9,12 @@ export default NuxtAuthHandler({
     adapter: PrismaAdapter(prisma),
     secret: process.env.NUXTAUTH_SECRET,
     providers: [
-        // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
-        GithubProvider.default({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET
-        }),
         // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point 
         GoogleProvider.default({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         }),
+
         // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
         CredentialsProvider.default({
             // The name to display on the sign in form (e.g. 'Sign in with...')
